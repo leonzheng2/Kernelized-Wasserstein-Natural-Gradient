9c56b518bf5d1b71ca85fe0d0b8e7c7007266c1c
Leon Zheng <leonpapanoel@gmail.com>
2019-12-10 00:23:45 +0100

    Initial commit

---
+++ KWNG.py
@@ -0,0 +1,131 @@
+"""
+Implementation of Kernelized Wasserstein NAtural Gradient
+Leon Zheng
+"""
+
+import numpy as np
+import scipy.linalg
+import math
+
+def gaussian_kernel(a, b, sigma):
+    """
+    Gaussian kernel
+    :param a: vector (d, )
+    :param b: vector (d, )
+    :param sigma: float
+    :return: float
+    """
+    return np.exp(- np.linalg.norm(a - b)**2 / (2*sigma**2))
+
+def grad_gaussian_kernel(k, l, a, b, sigma):
+    """
+    Compute \partial_k \partial_{l+d} k(a, b), for k, l \in [1,d]
+    :param k: int in [1,d]
+    :param l: int in [1,d]
+    :param a: vector (d, )
+    :param b: vector (d, )
+    :param sigma: float
+    :return: float
+    """
+    return 1/sigma**2 * gaussian_kernel(a, b, sigma) * (-1/sigma**2 * (a[k] - b[k]) * (a[l] - b[l]) + (l == k))
+
+"""
+Representation
+ * theta = [mu, delta]. Shape: (2, d).
+ * mu is the mean of the Gaussian. Shape: (d, )
+ * delta is the diagonal of the covariance matrix. Shape: (d, )
+"""
+
+def h_theta(theta, z):
+    """
+    Output x from latent variable z according the model of parameter theta, multivariate normal model.
+    :param theta: matrix (2, d)
+    :param z: vector (d, )
+    :return: vector (d, )
+    """
+    mu = theta[0]
+    delta = np.diag(theta[1])
+    root = scipy.linalg.sqrtm(delta)
+    return root @ z + mu
+
+def grad_theta_h_theta(theta, z):
+    """
+    Output the gradient of \theta \mapsto h_{\theta}(z), which is a jacobian matrix, shape (d, 2d).
+    :param theta: matrix (2, d)
+    :param z: vector (d, )
+    :return: matrix (d, 2d)
+    """
+    d = theta.shape[1]
+    delta = theta[1]
+    grad = np.zeros((d, 2*d))
+    for i in range(d):
+        grad[i, i] = 1
+        grad[i, d + i] = z[i] / (2 * np.sqrt(delta[i]))
+    return grad
+
+
+# Parameters of experiment
+d = 3
+N = 20
+M = math.floor(d * np.sqrt(N))
+lamb = 1e-10
+eps = 1e-10
+
+# Parameter of a random model
+mu = np.random.rand(d)
+delta = np.random.rand(d)
+theta = np.array([mu, delta])
+q = theta.shape[0] * theta.shape[1]
+
+# Samples
+Z = np.random.normal(size=(N, d))
+X = np.array([h_theta(theta, z) for z in Z])
+Y = X[np.random.randint(N, size=M)]
+idx = np.random.randint(d, size=M)
+
+# Gaussian kernel
+sigma_0 = np.float_power(10, 3.3)
+dist = np.array([[np.linalg.norm(X[n] - Y[m]) for n in range(N)] for m in range(M)])
+sigma_N_M = np.mean(dist)
+sigma = sigma_0 * sigma_N_M
+
+# Matrix C
+C = np.zeros((M, N * d))
+for m in range(M):
+    for n in range(N):
+        for i in range(d):
+            C[m, n * d + 1] = grad_gaussian_kernel(idx[m], i, Y[m], X[n], sigma)
+print(C.shape)
+
+# Matrix K
+K = np.zeros((M, M))
+for m1 in range(M):
+    for m2 in range(M):
+        K[m1, m2] = grad_gaussian_kernel(idx[m1], idx[m2], Y[idx[m1]], Y[idx[m2]], sigma)
+print(K.shape)
+
+# Matrix T
+T = np.zeros((M, q))
+for m in range(M):
+    for n in range(N):
+        grad_kernel = np.array([grad_gaussian_kernel(idx[m], i, Y[m], X[n], sigma) for i in range(d)])
+        grad_h = grad_theta_h_theta(theta, Z[n])
+        T[m, :] += grad_kernel.T @ grad_h
+    T[m, :] /= N
+print(T.shape)
+
+# Matrix D(theta)
+D = np.diag([np.linalg.norm(T[:, i])**2 for i in range(q)])
+print(D)
+print(D.shape)
+
+# Estimation of inverse matrix for wasserstein natural gradient
+D_inv = np.linalg.inv(D)
+matrix = T @ D_inv @ T.T + lamb * eps * K + eps / N * C @ C.T
+# print(matrix - matrix.T < 1e-15) # The matrix is indeed symetric in the computer
+w, v = np.linalg.eig(matrix)
+pseudo_inv = np.linalg.pinv(matrix)
+G_inv_hat = 1 / eps * (D_inv - D_inv @ T.T @ pseudo_inv @ T @ D_inv)
+print(G_inv_hat.shape)
+print(G_inv_hat)
+
